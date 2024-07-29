82b64f6166dd512e832874e9e89146a92baf1bec
Marco Lombardi <marco.lombardi@gmail.com>
2024-07-29 18:43:44 +0200

    Tests added

---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+using PrettyChairmarks
+using Test
+
+@testset "Pretty output" begin
+    buf = IOBuffer()
+    Base.show(buf, MIME("text/plain"), @bs sin(3.0))
+    s = String(take!(buf))
+    @test startswith(s, r"Chairmarks\.Benchmark: [0-9]* samples? with [0-9]* evaluations?\.")
+    @test endswith(s, "allocs estimate: 0.")
+    @test occursin("Histogram: log(frequency) by time", s)
+end
